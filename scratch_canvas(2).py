c10f9a64631af0ce9931d789c0737af3cbece356
Deven khichar <95070492+devenkhichar@users.noreply.github.com>
2022-04-12 16:19:22 +0530

    Add files via upload

---
+++ scratch_canvas(2).py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+T = [58, 62, 52, 60, 66, 74, 68, 80, 76, 74, 64]
+s= [215, 325, 185, 332, 406, 522, 412, 614, 544, 445, 408]
+plt.xlabel("temprature during sales")
+plt.ylabel("sales of the shopkeeper")
+plt.scatter(T,s)
+plt.show()
+
+
+
